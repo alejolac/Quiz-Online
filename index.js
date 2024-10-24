c27f33432fb584f084a0a8683d3dea56cf5b63c9
Alejo <102807501+alejolac@users.noreply.github.com>
2024-10-24 11:14:29 -0300

    first select with answers

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
 require ("dotenv").config()
 
 const connectionString = process.env.CONNECTION_STRING
 
-const client = new Client({
+const pool = new Pool({
     connectionString: connectionString,
 })
 
-client.connect()
+pool.connect()
   .then(() => {
     console.log('Conectado a la base de datos');
   })
   .catch(err => console.error('Error conectando a la base de datos', err.stack));
+
+pool.query("SELECT * FROM answers")
+  .then(res => {
+    console.log(res.rows)
+  })
+  .catch(err => console.error("Error al hacer la consulta", err.stack))
