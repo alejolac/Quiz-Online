1afb67de60b60a8981759d6b9ad1203c25f8e031
Alejo <102807501+alejolac@users.noreply.github.com>
2024-10-29 10:46:21 -0300

    quiz query 0.1

--- src/controllers/quizController.js
+++ src/controllers/quizController.js
@@ -37,6 +37,10 @@ const getAllCategories = async (req, res) => {
 const getAllQuizesCat = async (req, res) => {
     const { catId } = req.params 
 
+    if (catId === undefined) {
+        return res.status(400).json({ success: false, message: "El ID de la categoría es requerido" });
+    }
+
     try {
         const query = `
         SELECT q.*
@@ -59,4 +63,40 @@ const getAllQuizesCat = async (req, res) => {
     }
 }
 
-module.exports = { getAllQuiz, getAllCategories, getAllQuizesCat };
+const getQuizData = async (req, res) => {
+    const { quizId } = req.params 
+
+    if (quizId === undefined) {
+        return res.status(400).json({ success: false, message: "El ID del quiz es requerido" });
+    }
+
+    try {
+        const query = `
+            SELECT 
+                q.id AS question_id, 
+                q.content AS question_content, 
+                a.id AS answer_id, 
+                a.content AS answer_content, 
+                a.is_correct
+            FROM 
+                questions AS q
+            JOIN 
+                answers AS a ON q.id = a.question_id
+            WHERE 
+                q.quiz_id = $1;
+        `;
+        const result = await pool.query(query, [quizId]);
+
+        if (result.rows.length == 0) {
+            return res.status(404).json({ success: false, message: "No hay quizes de esa categoria" })
+        }
+
+        res.status(200).json({ success: true, message: result.rows })
+
+    } catch (err) {
+        console.error("Error al hacer la consulta", err.stack);
+        res.status(500).json({ error: "Error interno del servidor" });
+    }
+}
+
+module.exports = { getAllQuiz, getAllCategories, getAllQuizesCat, getQuizData };
--- src/routes/quizRoutes.js
+++ src/routes/quizRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express")
-const { getAllQuiz, getAllCategories, getAllQuizesCat } = require("../controllers/quizController")
+const { getAllQuiz, getAllCategories, getAllQuizesCat, getQuizData } = require("../controllers/quizController")
 
 const router = express.Router();
 
 router.get("", getAllQuiz);
 router.get("/categories", getAllCategories)
 router.get("/categories/:catId", getAllQuizesCat)
+router.get("/game/:quizId", getQuizData)
 
 module.exports = router;
